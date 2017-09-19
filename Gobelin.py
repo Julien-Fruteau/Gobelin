04e0e0478d160f3ca04c7979b3cc2976fdc389ca
Julien Fruteau <julien.fruteau@gmail.com>
2017-09-19 13:32:55 +0200

    To comply with data science course
    1-Extracted Randty to be created each time of a game iteration. I
    believe it it better to create it randomly once, and then run the game
    with that same gobelin X number of times.
    2-Added features for each Gobelin in order to return a set of features
    at the end of each game iteration. This is done to run some analysis
    later on.

--- Gobelin.py
+++ Gobelin.py
@@ -9,7 +9,7 @@
 
 import random
 import matplotlib.pyplot as plt
-# import numpy as np
+import numpy as np
 from collections import Counter
 import pandas as pd
 # import scipy.stats as ss
@@ -41,23 +41,38 @@ de l'extinction des gobelins.\n"""
 # FUNCTIONS _________________________________________________________________
 def create_gobelin(name):
     '''
-    Return a list with gobelin name, hitpoints which will evolve in combat,
-    min_damage, max_damage, initial hitpoints for later analysis
+    Return a list with:
+    gobelin name,
+    init hitpoints,
+    hp left which will evolve in combat,
+    min_damage, max_damage,
+    mean_dam_given, mean_dam_taken,
+    total_dam_given, total_dam_taken, hit_position.
     '''
-    hp = random.choice(range(min_hitpoints, max_hitpoints))
+    hp = random.randint(min_hitpoints, max_hitpoints)
     gobelin = [name,
                hp,
-               random.choice(range(min_damage_min, max_damage_min)),
-               random.choice(range(min_damage_max, max_damage_max)),
-               hp]
+               hp,
+               random.randint(min_damage_min, max_damage_min),
+               random.randint(min_damage_max, max_damage_max),
+               0, 0, 0, 0, 0]
     return gobelin
+
+
+def init_gobelin(gobelin):
+    gobelin[2] = gobelin[1]
+    gobelin[5] = 0
+    gobelin[6] = 0
+    gobelin[7] = 0
+    gobelin[8] = 0
+    gobelin[9] = 0
 
 
 def damage_by(gobelin):
     '''
     Return random damage between min and max damage for a given Gobelin
     '''
-    return random.randint(gobelin[2], gobelin[3])
+    return random.randint(gobelin[3], gobelin[4])
 
 
 def random_gobelin_hit(gobelins, gobelin):
@@ -71,46 +86,85 @@ def random_gobelin_hit(gobelins, gobelin):
     return random.choice(gobelin_to_hit)
 
 
-def handle_hitpoints(gobelins, gobelin, gobelin_hit, comments):
+def handle_hitpoints(gobelins, gobelin, gobelin_hit_id, combatround, comments):
     '''
     Module to adjust hitpoints of the gobelin_hit by gobelin.
 
     Since life cannot be negative, adjust to zero if result is negative.
 
-    If gobelin down, remove it from gobelins
+    If gobelin down, remove it from gobelins list
     '''
     damage = damage_by(gobelin)
-    gobelins[gobelin_hit][1] -= damage
+    gobelins[gobelin_hit_id][2] -= damage
 
-    if gobelins[gobelin_hit][1] < 0:
-        gobelins[gobelin_hit][1] = 0
+    # updates damage tracking:
+
+    # mean_dam_given => updated in the end
+
+    # mean_dam_taken (nb, this is temporary computing, in the end it will
+    # computed to the actual mean, using total damage taken [8] and this [6],
+    # which is temporarely the frequency at which the gobelin has been hit):
+    gobelins[gobelin_hit_id][6] += 1
+
+    # total_dam_given
+    gobelin[7] += damage
+    # total_dam_taken
+    gobelins[gobelin_hit_id][8] += damage
+
+    if gobelins[gobelin_hit_id][2] < 0:
+        gobelins[gobelin_hit_id][2] = 0
 
     if comments:
         print("{0} hits {1} for {2} damage. {1} has {3} hp left.".
-              format(gobelin[0], gobelins[gobelin_hit][0], damage,
-                     gobelins[gobelin_hit][1]))
+              format(gobelin[0], gobelins[gobelin_hit_id][0], damage,
+                     gobelins[gobelin_hit_id][2]))
 
-    if gobelins[gobelin_hit][1] == 0:
+    if gobelins[gobelin_hit_id][2] == 0:
         if comments:
             print("{0} falls in combat after {1} rounds".
-                  format(gobelins[gobelin_hit][0], combatround))
-        gobelins.remove(gobelins[gobelin_hit])
+                  format(gobelins[gobelin_hit_id][0], combatround))
+        gobelins.remove(gobelins[gobelin_hit_id])
 
 
-def run_game(comments=False):
+def run_game(Randty, comments=False):
     '''
     Return the winner and number of combatround
     '''
     # VARIABLES _____________________________________________________________
 
-    gobelins = [["Stinky", stinky_hitpoints, stinky_min_damage,
-                 stinky_max_damage, stinky_hitpoints],
-                ["Grunty", grunty_hitpoints, grunty_min_damage,
-                 grunty_max_damage, grunty_hitpoints],
-                ["Horrty", horrty_hitpoints, horrty_min_damage,
-                 horrty_max_damage, horrty_hitpoints]]
+    # each gobelin has :
+    #      "name", "hp0", "min_dam",
+    #      "max_dam", "hp_left,"
+    #      "mean_dam_given", "mean_dam_taken",
+    #      "total_dam_given", "total_dam_taken,
+    #      "hit_position"
 
-    gobelins.append(create_gobelin("Randty"))
+    gobelins = [["Stinky",
+                 stinky_hitpoints,
+                 stinky_hitpoints,
+                 stinky_min_damage,
+                 stinky_max_damage,
+                 0, 0, 0, 0, 0],
+
+                ["Grunty",
+                 grunty_hitpoints,
+                 grunty_hitpoints,
+                 grunty_min_damage,
+                 grunty_max_damage,
+                 0, 0, 0, 0, 0],
+
+                ["Horrty",
+                 horrty_hitpoints,
+                 horrty_hitpoints,
+                 horrty_min_damage,
+                 horrty_max_damage,
+                 0, 0, 0, 0, 0]]
+
+    #  Does the random gobelins has to be randomly created at each
+    # game iteration. Not sure, let's pass Randy as a param of run_game instead:
+    # gobelins.append(create_gobelin("Randty"))
+    init_gobelin(Randty)
+    gobelins.append(Randty)
 
     # gobelins = [create_gobelin(name) for name in gobelins_name]
 
@@ -122,6 +176,10 @@ def run_game(comments=False):
         print(intro)
         for gobelin in gobelins:
             print("{0} has {1} hitpoints.".format(gobelin[0], gobelin[1]))
+    for i, gobelin in enumerate(gobelins):
+        gobelin[9] = i + 1
+
+    total_players = len(gobelins)
 
     # MAIN LOOP _____________________________________________________________
     while len(gobelins) > 1:
@@ -133,18 +191,29 @@ def run_game(comments=False):
 
             gobelin_hit = random_gobelin_hit(gobelins, i)
 
-            handle_hitpoints(gobelins, gobelin, gobelin_hit, comments)
+            handle_hitpoints(gobelins, gobelin, gobelin_hit,
+                             combatround, comments)
     # END GAME ______________________________________________________________
     if comments:
         print("==================================")
         print("The combat ends after %i rounds" % combatround)
         print(gobelins[0][0] + " is the winner !")
-    return(gobelins[0][0], gobelins[0][4],
-           gobelins[0][2], gobelins[0][3], combatround)
+
+    # mean_dam_given by winner:
+    gobelin[5] = gobelin[7] / combatround
+    # mean_dam_taken by winner:
+    gobelin[6] = gobelin[8] / gobelin[6]  # (seen handle_hitpoints())
+
+    return(gobelins[0][0], gobelins[0][1], gobelins[0][2],
+           gobelins[0][3], gobelins[0][4],
+           gobelins[0][5], gobelins[0][6],
+           gobelins[0][7], gobelins[0][8],
+           combatround, gobelins[0][9], total_players)
 
 
 # GLOBALS ___________________________________________________________________
-# # définition de Grunty et Stinky. Notez que les variables sont en minuscules
+# # définition de Grunty, Stinky, Horrty.
+# # Notez que les variables sont en minuscules
 stinky_hitpoints = 50  # Stinky est faible
 grunty_hitpoints = 60  # Grunty est fort
 horrty_hitpoints = 40
@@ -158,7 +227,8 @@ grunty_max_damage = 6  # mais plus de degats au maximum
 horrty_min_damage = 2
 horrty_max_damage = 5
 
-# Gobelin Population characteristics:
+# Gobelin Population characteristics to be chosen randomly from by
+# create_gobelin():
 min_hitpoints = 1
 max_hitpoints = 101
 
@@ -172,33 +242,58 @@ max_damage_max = 9
 
 
 # ANALYSIS __________________________________________________________________
+
+
+# means = np.empty((0, 2))
+
+# victories = pd.DataFrame(
+# columns=("winner", "hp", "min_dam", "max_dam", "combatround"))
+
 victories = pd.DataFrame(
-    columns=("winner", "hp", "min_dam", "max_dam", "combatround"))
+    columns=("winner", "hp", "hp_left",
+             "min_dam", "max_dam",
+             "mean_dam_given", "mean_dam_taken",
+             "total_dam_given", "total_dam_taken",
+             "combatround", "hit_position", "total_players"))
 
+# here you can create loop (with below nested loop)
+Ranty = create_gobelin("Randty")
+
+# how does the random gobelin Randty performs in X games?:
 for rep in range(10):
-    # (winner, hp, min_dam, max_dam, combatround) = run_game()
-    victories.loc[rep + 1] = run_game()
+    victories.loc[rep + 1] = run_game(Ranty)
+    # randty_wins = victories[victories.winner == "Randty"]
+    # hp_mean = randty_wins.hp.mean()
+    # damage_mean = (randty_wins.min_dam.mean() +
+    #                randty_wins.max_dam.mean()) / 2
+    # means = np.concatenate(
+    #     (means, np.array([[hp_mean, damage_mean]])), axis=0)
 
-
+# Check victorires:
+victories.head()
+# Check our random gobelin Randty:
+Ranty
 randty_wins = victories[victories.winner == "Randty"]
-
-# shows the frequency of wins for each gobelin
-win_freq = Counter(victories.winner)
-plt.bar(range(len(win_freq)), win_freq.values())
-plt.xticks(range(len(win_freq)), win_freq.keys())
-plt.show()
-
-# Rules:
-# How many gobelins fight? 4, given 3 gobelins and a random gobelin at each
-# game iteration.
-
-# given a gobelin population, which gobelin characteristic will win the most
-# and which one will loose the most?
-
-# it is obvious that a gobelin having 100 hp and [4-8] damages will
-# win the most.
-# hence to deinie a population, we can sample from a normal distribution
-# with mean the average of min-max hp/damage_min/damage_max:
+randty_wins
 
 
-# how can the population would mutate towards the most frequent winner?
+# randty_wins.head()
+
+# # shows the frequency of wins for each gobelin
+# win_freq = Counter(victories.winner)
+# win_freq
+# plt.bar(range(len(win_freq)), win_freq.values())
+# plt.xticks(range(len(win_freq)), win_freq.keys())
+# plt.show()
+
+# display the characteristics of randty, for all his wins
+# plt.plot(randty_wins.hp, randty_wins.min_dam, 'ro')
+# plt.plot(randty_wins.hp, randty_wins.max_dam, 'bo')
+# plt.show()
+
+# # over rep increasing to high number, are randty carac trending towards
+# # certain carac?
+# plt.figure()
+# plt.plot(means[:, 0], 'ro')
+# plt.plot(means[:, 1], 'bo')
+# plt.show()
